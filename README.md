dcefa3f8a2d26c058bfaf0bbfcc55f28609ee924
w-udhav <udhavwadhawan@hotmail.com>
2023-05-09 14:14:33 +0530

    new: ready to work

---
+++ README.md
@@ -0,0 +1,1 @@
+# CRM - for Education
---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>React 18 Alpha</h1>
+      <p>React 18 Alpha is here!</p>
+      <p className="text-red-500">This is a red paragraph.</p>
+    </div>
+  );
+}
+
+export default App;
